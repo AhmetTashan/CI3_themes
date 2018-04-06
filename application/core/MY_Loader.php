da520677325a1757afba82a881a57efacb68e310
Ahmet Taşhan <ahmettashan@outlook.com>
2018-04-06 16:17:11 +0200

    Create MY_Loader.php

---
+++ application/core/MY_Loader.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Loader extends CI_Loader
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+
+	}
+
+	public function template($template_name, $vars = array(), $return = FALSE)
+	{
+		$this->_ci_view_paths = array( $_SERVER['DOCUMENT_ROOT'].'/themes/default/view/' => 1);
+
+		@$content  = $this->view('includes/header', $vars, $return);
+		@$content .= $this->view('includes/navbar', $vars, $return);
+		@$content .= $this->view($template_name, $vars, $return);
+		@$content .= $this->view('includes/footer', $vars, $return);
+
+		if ( $return )
+		{
+			return $content;
+		}
+	}
+
+}
